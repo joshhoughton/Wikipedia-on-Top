5ebcf2be032bc2b748815447c573e2a6785a9467
Josh Houghton <j@joshhoughton.io>
2024-03-04 23:55:52 +0000

    Rewrite injection

--- wot/src/inject/inject.js
+++ wot/src/inject/inject.js
@@ -1,7 +1,26 @@
-$("div#search > div > div > div")
-    .filter((i, el) => el.outerHTML.includes("wikipedia."))
-    .each((i, el) => {
-        if (i === 0) {
-            $(el).insertBefore($("div#search > div > div > div").first())
-        }
-    })
+(function moveFirstWikipediaResultToTop() {
+    // Main container of all search results
+    const mainContainer = document.querySelector('#rso');
+  
+    const searchResults = document.querySelectorAll('.g');
+  
+    let firstWikipediaResult = null;
+  
+    for (let result of searchResults) {
+      // URL of the search result
+      const resultUrl = result.querySelector('.yuRUbf a')?.href;
+
+      if (resultUrl && resultUrl.includes('wikipedia.org')) {
+        firstWikipediaResult = result;
+        break;
+      }
+    }
+  
+    // If a Wikipedia result was found, move it to the top
+    if (firstWikipediaResult) {
+      mainContainer.prepend(firstWikipediaResult);
+      console.log('Moved the first Wikipedia result to the top.');
+    } else {
+      console.log('No Wikipedia results found.');
+    }
+  })();
