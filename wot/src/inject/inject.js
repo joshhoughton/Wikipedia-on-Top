6fff255fb01b84560d66cfcf4ffcedddf08b9f8b
Josh Houghton <j@joshhoughton.io>
2025-03-29 12:32:45 +0000

    Update to work with new Google search page layout

--- wot/src/inject/inject.js
+++ wot/src/inject/inject.js
@@ -2,18 +2,42 @@
   // Main container of all search results
   const mainContainer = document.querySelector("#rso");
 
-  const searchResults = document.querySelectorAll(".g");
+  if (!mainContainer) {
+    console.log("Main container (#rso) not found.");
+    return;
+  }
+
+  // Try different selectors for search results
+  const selectors = [".MjjYud", ".hlcw0c", ".g"];
+  let searchResults = [];
+
+  // Try each selector until we find results
+  for (const selector of selectors) {
+    searchResults = document.querySelectorAll(selector);
+    if (searchResults.length > 0) {
+      console.log(`Using selector: ${selector}`);
+      break;
+    }
+  }
+
+  if (searchResults.length === 0) {
+    console.log("No search results found with any of the known selectors.");
+    return;
+  }
 
   let firstWikipediaResult = null;
 
   for (let result of searchResults) {
-    // URL of the search result
-    const resultUrl = result.querySelector(".yuRUbf a")?.href;
+    const links = result.querySelectorAll("a");
 
-    if (resultUrl && resultUrl.includes("wikipedia.org")) {
-      firstWikipediaResult = result;
-      break;
+    for (const link of links) {
+      if (link.href && link.href.includes("wikipedia.org")) {
+        firstWikipediaResult = result;
+        break;
+      }
     }
+
+    if (firstWikipediaResult) break;
   }
 
   // If a Wikipedia result was found, move it to the top
